41fff9ad7f5b8817449cd03c855097f0f37d3b4f
Dilkhush Kumar <rajdilkhush0987654321@gmail.com>
2025-07-24 19:21:28 +0530

    fix lockfiles

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
-import {defineConfig} from "vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
@@ -9,14 +9,12 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  server: {
-    port: 5173,
-    host: true,
-    allowedHosts: ["3f58cd1d2dda.ngrok-free.app"],
-  },
   preview: {
     allowedHosts: ["oorooree.com"],
     port: 4173,
     host: true,
+  },
+  build: {
+    outDir: "build",
   },
 });
